f130193c183877354d4f90bdfd93f60620bcd86c
Amr Abdeen <amr.abdeen2050@yahoo.com>
2017-07-03 08:07:17 -0700

    Update AddFunc-Struct.c

--- AddFunc-Struct.c
+++ AddFunc-Struct.c
@@ -30,21 +30,21 @@ struct value add(struct value val1, struct value val2, enum dataType type)
 	return result;
 }
 int main() {
-	struct value U1, U2, Result;
+	struct value S1, S2, Result;
 	// INT data type
-	U1.u_intValue = 5000;
-	U2.u_intValue = 8000;
-	Result = add(U1, U2, INT);
+	S1.u_intValue = 5000;
+	S2.u_intValue = 8000;
+	Result = add(S1, S2, INT);
 	printf("Integer Addition result is : %d \n", Result.u_intValue);
 	//FLOAT data type
-	U1.u_floatValue = 50.2f;
-	U2.u_floatValue = 67.5f;
-	Result = add(U1, U2, FLOAT);
+	S1.u_floatValue = 50.2f;
+	S2.u_floatValue = 67.5f;
+	Result = add(S1, S2, FLOAT);
 	printf("Float Addition result is : %f \n", Result.u_floatValue);
 	//DOUBLE data Type
-	U1.u_doubleValue = 300.22;
-	U2.u_doubleValue = 805.12;
-	Result = add(U1, U2, DOUBLE);
+	S1.u_doubleValue = 300.22;
+	S2.u_doubleValue = 805.12;
+	Result = add(S1, S2, DOUBLE);
 	printf("Double Addition result is : %lf \n", Result.u_doubleValue);
 	// Uncomment if you're using VS
 	//getchar();
