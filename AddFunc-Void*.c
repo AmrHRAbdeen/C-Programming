4c698138ea3e6f228aa5eab81d65925adf3b4e61
Amr Abdeen <amr.abdeen@iedu-eg.com>
2017-07-05 06:45:38 -0700

    #1-optimization

--- AddFunc-Void*.c
+++ AddFunc-Void*.c
@@ -3,7 +3,7 @@ Author : Amr Abdeen
 About :One function to add various data types using Unknown pointer.
 **Pros and Cons of this Method:
 -->Pros:
-	- Functionality:The best one;as it depends on passing by reference so the code size is smaller and the process is faster 
+	- Functionality:The best one;as it depends on passing by reference so the code size is smaller and the process is faster
 			than the other two methods.
 -->Cons:
 	-Not Found.
@@ -19,28 +19,28 @@ int main(){
 	int a =50;
 	int b =500;
 	int result;
-	void* PTR_a= &a;
-	void* PTR_b= &b;
-	void* PTR_result= &result;
-	add(PTR_a,PTR_b,PTR_result,INT);
+	//void* PTR_a= &a;
+	//void* PTR_b= &b;
+	//void* PTR_result= &result;
+	add(&a,&b,&result,INT);
 	printf("Result is : %d \n",result);
 	//FLOAT data Type
 	float float_a =50.5;
 	float float_b =500.66;
 	float float_result;
-	void* FPTR_a= &float_a;
-	void* FPTR_b= &float_b;
-	void* FPTR_result= &float_result;
-	add(FPTR_a,FPTR_b,FPTR_result,FLOAT);
+	//void* FPTR_a= &float_a;
+	//void* FPTR_b= &float_b;
+	//void* FPTR_result= &float_result;
+	add(&float_a,&float_b,&float_result,FLOAT);
 	printf("Result is : %f \n",float_result);
 	//Double Data Type
 	double double_a =50.663;
 	double double_b =500.3368;
 	double double_result;
-	void* DPTR_a=&double_a;
-	void* DPTR_b=&double_b;
-	void* DPTR_result=&double_result;
-	add(DPTR_a,DPTR_b,DPTR_result,DOUBLE);
+	//void* DPTR_a=&double_a;
+	//void* DPTR_b=&double_b;
+	//void* DPTR_result=&double_result;
+	add(&double_a,&double_b,&double_result,DOUBLE);
 	printf("Result is : %lf \n",double_result);
 	return 0;
 }
