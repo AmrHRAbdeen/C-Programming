8af96e2d89e2f4a975d99db20827f9757e4190dc
Amr Abdeen <amr.abdeen2050@gmail.com>
2018-01-04 07:47:28 -0800

    indentation

--- CountingDigitsandSpaces.c
+++ CountingDigitsandSpaces.c
@@ -6,13 +6,16 @@ int main(int argc, char const *argv[])
 	int ndigits[10];
 	nwhite = nother=0;
 	for(i=0;i<10;++i) ndigits[i]=0;
+	//EOF = End of file , you can end a file with CTRL+Z in windows ,followed by ENTER if necessary
 	while((c=getchar()) != EOF)
+	{
 		if(c>= '0' && c <= '9')
 			++ndigits[c-'0'];	
 		else if (c==' ' || c=='\n' || c=='\t')
 			++nwhite;
 		else
 			++nother;
+	}	
 	printf("digits= ");	
 	for(i=0;i<10;++i)
 		printf(" %d",ndigits[i]);
