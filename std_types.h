a98afa31e36e0c0df458d0bd1f70c1a9a81cca0a
Amr Abdeen <amr.abdeen2050@gmail.com>
2018-12-16 14:34:55 +0200

    Create std_types.h

---
+++ std_types.h
@@ -0,0 +1,20 @@
+/*
+************************************
+Standard Data Types
+Check for every tool chain first...
+Author @ Amr Abdeen
+************************************
+*/
+#ifndef STD_TYPES_H_INCLUDED
+#define STD_TYPES_H_INCLUDED
+
+typedef unsigned char      u8 ;
+typedef unsigned short int u16;
+typedef unsigned long int  u32;
+typedef signed char        s8 ;
+typedef signed short int   s16;
+typedef signed long int    s32;
+typedef float              f32;
+typedef long               f64;
+
+#endif // STD_TYPES_H_INCLUDED
