60ba823700afb3da0ddedf6aa34742e2fc9f3eb9
Amr Abdeen <amr.abdeen2050@gmail.com>
2018-12-16 14:16:43 +0200

    Update circularShift.c

--- circularShift.c
+++ circularShift.c
@@ -1,11 +1,13 @@
 /**********************************************************************************************************
-                                        *****Circlar Left Shift*****
+                                        *****Circlar Left Shift*****                                     
                        -This code is about circulat Left shift of an 8 bit variable (CHAR) 
                        -Cirular Left Shift is done by 3 Steps:
                                 1-Shifting the 8 bit var to Left by the Amount of shit
                                 2-Shifting the 8 bit var to right by: 
                                    [ ( NumberOfBits in the variable (i.e: 8 in this case)*sizeof(x) )-AmountOfShift]
                                 3- ORing step 1 with step 2 i.e: (result of step 1) | (result of step 3)
+
+
 ************************************************************************************************************/
 
 #include <stdio.h>
@@ -17,7 +19,8 @@ typedef unsigned char u8;
 
 /**************************Defintion of Circular Left Shift Function*********************************
     -Takes the variable , amount of left shift and returns the new shifted and circulated variable
-    
+    -INPUTS:   Var        : Variable to be shifted
+               shiftAmount: shift amount
 /********************************************************************************************/
 
 int cirShiftToLeft(int x ,int shiftAmount){
