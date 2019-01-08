dcde4d84f2e95e97e2b9c8b3341f5f4c708e66c3
Amr Abdeen <amr.abdeen2050@gmail.com>
2019-01-08 17:18:24 +0200

    Update std_types.h

--- std_types.h
+++ std_types.h
@@ -15,6 +15,6 @@ typedef signed char        s8 ;
 typedef signed short int   s16;
 typedef signed long int    s32;
 typedef float              f32;
-typedef long               f64;
+typedef double               f64;
 
 #endif // STD_TYPES_H_INCLUDED
